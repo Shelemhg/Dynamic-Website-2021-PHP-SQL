0d57a2c346ac062210d7dc6275230cb7f71bb1db
Shelemhg <48326251+Shelemhg@users.noreply.github.com>
2022-03-06 22:41:03 -0600

    I HATE PROGRAMMING: Password Update fixed after hashing the new password typed, not the response from passwordCheck function

--- accounts/index.php
+++ accounts/index.php
@@ -62,6 +62,7 @@ switch ($action) {
         $clientEmail = trim(filter_input(INPUT_POST, 'clientEmail', FILTER_SANITIZE_EMAIL));
         $clientEmail = checkEmail($clientEmail);
         $clientPassword = filter_input(INPUT_POST, 'clientPassword', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
         $passwordCheck = checkPassword($clientPassword);
 
         // Run basic checks, return if errors
@@ -75,19 +76,21 @@ switch ($action) {
         // A valid password exists, proceed with the login process
         // Query the client data based on the email address
         $clientData = getClient($clientEmail);
+
+        $storedPassword = $clientData['clientPassword'];
         // Compare the password just submitted against
         // the hashed password for the matching client
-        $hashCheck = password_verify($clientPassword, $clientData['clientPassword']);
+        $hashCheck = password_verify($clientPassword, $storedPassword);
         
         // If the hashes don't match create an error
         // and return to the login view
         if(!$hashCheck){
             $pageTitle = 'Login';
-            // $message = '<p class="notice">Please check your password and try again.</p>';
-            $message = "<p>NO MATCH '$clientEmail' , '$clientPassword', pwd </p>";
+            $message = '<p class="notice">Please check your password and try again.</p>';
             include '../view/login.php';
             exit;
         }
+
         // A valid user exists, log them in
         $_SESSION['loggedin'] = TRUE;
         // Remove the password from the array
@@ -101,7 +104,7 @@ switch ($action) {
                                     Welcome {$clientData['clientFirstname']}
                                 </a>";
         // Send them to the admin view
-        $message = "<p>MATCH '$clientEmail' , '$clientPassword'</p>";
+        
         include '../view/admin.php';
         exit;
         break;
@@ -131,7 +134,8 @@ switch ($action) {
         $clientFirstname = trim(filter_input(INPUT_POST, 'clientFirstname', FILTER_SANITIZE_STRING));
         $clientLastname = trim(filter_input(INPUT_POST, 'clientLastname', FILTER_SANITIZE_STRING));
         $clientEmail = trim(filter_input(INPUT_POST, 'clientEmail', FILTER_SANITIZE_EMAIL));
-        $clientPassword = trim(filter_input(INPUT_POST, 'clientPassword', FILTER_SANITIZE_STRING));
+        $clientPassword = filter_input(INPUT_POST, 'clientPassword', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
         $clientEmail = checkEmail($clientEmail);
         $checkPassword = checkPassword($clientPassword);
 
@@ -249,10 +253,10 @@ switch ($action) {
     case 'updatePassword':
         // Filter and store the data
         $clientId = filter_input(INPUT_POST, 'clientId', FILTER_SANITIZE_NUMBER_INT);
-
+        unset($clientPassword);
         $clientPassword = filter_input(INPUT_POST, 'clientPassword', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $checkPassword = checkPassword($clientPassword);
-            
+
+        $checkPassword = checkPassword($clientPassword);            
 
         // Check for missing data
         if(empty($checkPassword)) {
@@ -260,8 +264,8 @@ switch ($action) {
             include $_SERVER['DOCUMENT_ROOT'] .'/phpmotors/view/client-update.php';
             exit; 
         }
-        // Hash the checked password
-        $hashedPassword = password_hash($checkPassword, PASSWORD_DEFAULT);
+        // Hash the client password if the above if didn't exit the program
+        $hashedPassword = password_hash($clientPassword, PASSWORD_DEFAULT);
 
         $updatedPassword = updatePassword($hashedPassword, $clientId);
         // Check and report the result
@@ -273,27 +277,9 @@ switch ($action) {
             exit;
         } else {
             $message = "<p>Password update failed. Please try again.</p>";
-            // $message = "<p>CFN '$clientFirstname', CLN '$clientLastname', CE '$clientEmail', clientId: '$clientId',  NEW STUFF</p>";
             include $_SERVER['DOCUMENT_ROOT'] .'/phpmotors/view/client-update.php';
             exit;
         }
-
-        // // Hash the checked password
-        // $hashedPassword = password_hash($clientPassword, PASSWORD_DEFAULT);
-        // // Send the data to the model
-        // $regOutcome = regClient($clientFirstname, $clientLastname, $clientEmail, $hashedPassword);
-        // // Check and report the result
-        // if($regOutcome === 1){
-        //     setcookie('firstname', $clientFirstname, strtotime('+1 year'), '/');
-        //     $_SESSION['message'] = "Thanks for registering $clientFirstname. Please use your email and password to login.";
-        //     header('Location: /phpmotors/accounts/?action=login');
-        //     // include $_SERVER['DOCUMENT_ROOT'] .'/phpmotors/view/login.php';
-        //     exit;
-        // } else {
-        //     $message = "<p>Sorry $clientFirstname, but the registration failed. Please try again.</p>";
-        //     include $_SERVER['DOCUMENT_ROOT'] .'/phpmotors/view/registration.php';
-        //     exit;
-        // }
         break;
     
     default:
